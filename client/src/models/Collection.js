bb8768e9497d4b3ff80df33f091591e1cb1761fd
MeganHancock <mgnhnck@gmail.com>
2024-03-15 13:47:23 -0600

    client collection model

---
+++ client/src/models/Collection.js
@@ -0,0 +1,8 @@
+export class Collection{
+    constructor(data){
+        this.title = data.title
+        this.coverImg = data.coverImg
+        this.creatorId = data.creatorId
+        this.creator = data.creator
+    }
+}
