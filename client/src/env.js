c26eb47b69339e495458a9cb70101672f5e3143b
MeganHancock <mgnhnck@gmail.com>
2024-03-19 13:45:51 -0600

    env

--- client/src/env.js
+++ client/src/env.js
@@ -4,4 +4,4 @@ export const baseURL = dev ? 'http://localhost:3000/' : ''
 export const useSockets = false
 export const domain = 'dev-lorj4jkzij36pzfj.us.auth0.com'
 export const clientId = 'jF1JOKaaT4m5JGHlgHs6PzmQMerYalnY'
-export const audience = 'https://joaquinapi.com/'
+export const audience = 'https://joaquinapi.com'
