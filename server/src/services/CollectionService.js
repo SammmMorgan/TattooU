3735d45dcc206ebaa9a71a33eaac596224dbc75a
Zac <step7476@alumni.uidaho.edu>
2024-03-19 14:36:29 -0600

    add to collection started

--- server/src/services/CollectionService.js
+++ server/src/services/CollectionService.js
@@ -13,26 +13,28 @@ class CollectionService {
         await collection.populate('imageCount')
         return collection
     }
-    
+
     async getCollectionById(id) {
         const collection = await dbContext.Collections.findById(id)
-        if(!collection) throw new Error(`No Collection was found with id ${id}`)
+        if (!collection) throw new Error(`No Collection was found with id ${id}`)
         return collection
     }
-    
+
     async updateCollection(id, data, userId) {
         const collection = await this.getCollectionById(id)
-        if(collection.creatorId != userId) throw new Forbidden('You are not the creator of this collection')
+        if (collection.creatorId != userId) throw new Forbidden('You are not the creator of this collection')
         collection.title = data.name || collection.title
         collection.coverImg = data.coverImg || collection.coverImg
         await collection.save()
         return collection
     }
-    
+
+    async addToCollection() { }
+
     async deleteCollection(id, userId) {
         const collection = await dbContext.Collections.findById(id)
-        if(!collection) throw new BadRequest(`Invalid id: ${id}`)
-        if(collection.creatorId != userId) throw new Forbidden(`Access Denied`)
+        if (!collection) throw new BadRequest(`Invalid id: ${id}`)
+        if (collection.creatorId != userId) throw new Forbidden(`Access Denied`)
         await collection.deleteOne()
         return `Collection Deleted`
     }
