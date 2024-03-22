913b9685aeec0a76d5f303d811298b64d91bd4a6
Joaquin Chavez <jchav5601@gmail.com>
2024-03-22 11:00:26 -0600

    Fixed Modal Issue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="screen">
-    <header>
-      <Navbar />
-    </header>
-    <main>
-      <router-view />
-    </main>
-    <!-- <footer class="bg-dark text-light">
+  <header>
+    <Navbar />
+  </header>
+  <main>
+    <router-view />
+  </main>
+  <!-- <footer class="bg-dark text-light">
     Made with 💖 by CodeWorks
   </footer> -->
-  </div>
 </template>
 
 <script>
